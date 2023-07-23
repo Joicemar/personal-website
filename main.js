acd09fcf221e8c1b5e95c17a3a0d821e0c22b494
Joicemar <33008360+Joicemar@users.noreply.github.com>
2023-07-23 03:48:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 
+
+
+
+let doc = document.querySelector('html');
+doc.classList.remove('no_js');
+doc.classList.add('active_js');
 /**
  * Botao de retornar
  */
-var returntop = document.getElementById("return-top");
+let returntop = document.getElementById("return-top");
 function returnTop() {
     var windowHeight = window.innerHeight || document.documentElement.clientHeight;//altura da janela atual
     var maxWidth = window.innerWidth || document.documentElement.clientWidth;//largura da janela atual 
@@ -72,37 +78,7 @@ boxes.forEach(box => {
 if ('scrollRestoration' in history) {
     history.scrollRestoration = 'manual';
 }
-/**
- * Imagem Animation; elemento Imagem acompanhar um pouco a rolagem
- 
-var blob = document.getElementById("blob");
-var distanciaTopo = blob.getBoundingClientRect().top;
 
-function followScroll() {
-    var posicaoTopo = blob.getBoundingClientRect().top;
-    var diferencaRolagem = posicaoTopo - distanciaTopo;
-    var scrollY = window.innerHeight || document.documentElement.clientHeight;
-    const width = window.innerWidth || document.documentElement.clientWidth;
-    var distanceScroll = window.screenY || document.documentElement.scrollTop;
-    var scrollPassou80vh = window.scrollY > (window.innerHeight * 0.45);
-    var somador = diferencaRolagem / 2;
-    //Para nova verificacao se passar scrolagem do header
-    var larguraTotal = window.innerHeight;
-    var deslocamentoVertical = window.pageYOffset || document.documentElement.scrollTop;
-    //var quantidade_px_para_fazer = larguraTotal - 421;
-
-    if (scrollY > distanceScroll && !scrollPassou80vh &&  deslocamentoVertical < larguraTotal && width > 500)  {
-        blob.style.marginTop = -diferencaRolagem - somador + 'px';
-    } else {
-        blob.style.marginTop = "0px;"
-    }
-}
-window.addEventListener('scroll', followScroll); 
-*/
-
-/**
- * Icon Menu
-*/
 //toggle icon
 var menu = document.getElementById('nav-link');
 document.getElementById('toggle').onclick = () => {
@@ -129,6 +105,107 @@ darkLight.onclick = () => {
         changeTheme.classList.toggle('ligado');
     }
 }
+/*escurecer página*/
+function escurecer() {
+    let larguraTotal = window.innerHeight;
+    let deslocamentoVertical = window.pageYOffset || document.documentElement.scrollTop;
+    let quantidade_px_para_fazer = larguraTotal - 421;
+    let headerContainer = document.querySelector('.header-container');
+
+    if (deslocamentoVertical > quantidade_px_para_fazer) {
+        let porcentagemRestante = (deslocamentoVertical - quantidade_px_para_fazer) / (larguraTotal - quantidade_px_para_fazer);
+        let opacidade = 1 - porcentagemRestante - (porcentagemRestante / 2);
+        headerContainer.style.opacity = opacidade.toFixed(2);
+    } else {
+        headerContainer.style.opacity = 1;
+    }
+}
+
+window.addEventListener('scroll', escurecer);
+
+/**
+* Loading Effect
+*/
+// Remove a classe "loading-overlay" após o carregamento do site
+window.addEventListener('load', function () {
+    let overlay = document.querySelector('.loading-overlay');
+    overlay.style.display = 'none';
+});
+
+
+//Secondary Cursor
+let cursor = document.getElementById('cursor');
+
+const body = document.querySelector('body');
+cursor.classList.add('active__js');
+cursor.classList.add('visible');
+
+function updateCursorPosition(e) {
+    requestAnimationFrame(function () {
+        cursor.style.left = e.pageX + 'px';
+        cursor.style.top = e.pageY + 'px';
+    });
+}
+
+document.addEventListener('mousemove', updateCursorPosition);
+window.addEventListener('scroll', function () {
+    cursor.classList.remove('visible');
+});
+window.addEventListener('mousemove', function () {
+    cursor.classList.add('visible');
+});
+
+let maxwidth = window.innerWidth;
+
+document.addEventListener('mouseleave', () => {
+    cursor.classList.remove('visible');
+});
+document.addEventListener('mouseenter', () => {
+    cursor.classList.add('visible');
+});
+
+// Inicializar o ScrollReveal
+const sr = ScrollReveal({
+    distance: '100px',
+    duration: 1300,
+    reset: true
+})
+sr.reveal('#contact,about-tittle, .about-tittle, .about, .about-gri, .about-description, .about-resume, .main-text', { delay: 50, origin: 'top' })
+sr.reveal(' .row', { delay: 50, origin: 'bottom' })
+sr.reveal(' .one', { delay: 50, origin: 'top' })
+sr.reveal(' .two', { delay: 50, origin: 'top' })
+sr.reveal(' .three', { delay: 50, origin: 'top' })
+/**
+ * Imagem Animation; elemento Imagem acompanhar um pouco a rolagem
+ 
+let blob = document.querySelector('blobs');
+let distanciaTopo = blob.getBoundingClientRect().top;
+
+function followScroll() {
+    let posicaoTopo = blob.getBoundingClientRect().top;
+    let diferencaRolagem = posicaoTopo - distanciaTopo;
+    let scrollY = window.innerHeight || document.documentElement.clientHeight;
+    let width = window.innerWidth || document.documentElement.clientWidth;
+    let distanceScroll = window.screenY || document.documentElement.scrollTop;
+    let scrollPassou80vh = window.scrollY > (window.innerHeight * 0.45);
+    let somador = diferencaRolagem / 2;
+    //Para nova verificacao se passar scrolagem do header
+    let larguraTotal = window.innerHeight;
+    let deslocamentoVertical = window.pageYOffset || document.documentElement.scrollTop;
+    //var quantidade_px_para_fazer = larguraTotal - 421;
+
+    if (scrollY > distanceScroll && !scrollPassou80vh &&  deslocamentoVertical < larguraTotal && width > 500)  {
+        blob.style.marginTop = -diferencaRolagem - somador + 'px';
+    } else {
+        blob.style.marginTop = "0px;"
+    }
+}
+window.addEventListener('scroll', followScroll); 
+*/
+
+/**
+ * Icon Menu
+*/
 /**
  * Light Effect inacabado
 document.getElementsByClassName("luz") = () => {
